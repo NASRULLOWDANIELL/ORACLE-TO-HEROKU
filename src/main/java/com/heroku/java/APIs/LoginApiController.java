627c15c5ea19d1c5e25cde468e1a1807fa8b4e52
NASRULLOWDANIELL <2022456146@student.uitm.edu.my>
2024-07-10 08:03:42 +0800

    include email n pass for output api login

--- src/main/java/com/heroku/java/APIs/LoginApiController.java
+++ src/main/java/com/heroku/java/APIs/LoginApiController.java
@@ -7,6 +7,7 @@ import com.heroku.java.bean.Customer;
 import com.heroku.java.bean.Staff;
 import com.heroku.java.bean.LoginRequest;
 import com.heroku.java.bean.LoginResponse;
+
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.http.ResponseEntity;
@@ -31,16 +32,17 @@ public class LoginApiController {
             Customer customer = customerDAO.getCustomerByCustemail(loginRequest.getEmail());
             if (customer == null) {
                 return ResponseEntity.status(HttpStatus.UNAUTHORIZED)
-                    .body(new LoginResponse(false, "Email does not exist"));
+                    .body(new LoginResponse(false, "Email does not exist", null, null, null));
             }
             if (!verifyPassword(loginRequest.getPassword(), customer.getCustpassword())) {
                 return ResponseEntity.status(HttpStatus.UNAUTHORIZED)
-                    .body(new LoginResponse(false, "Incorrect password"));
+                    .body(new LoginResponse(false, "Incorrect password", null, null, null));
             }
-            return ResponseEntity.ok(new LoginResponse(true, "Login successful"));
+            return ResponseEntity.ok(new LoginResponse(true, "Login successful", 
+                customer.getCustemail(), customer.getCustid(), customer.getCustpassword()));
         } catch (SQLException e) {
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
-                .body(new LoginResponse(false, "An error occurred"));
+                .body(new LoginResponse(false, "An error occurred", null, null, null));
         }
     }
 
@@ -51,16 +53,17 @@ public class LoginApiController {
             Staff staff = staffDAO.getStaffByStaffemail(loginRequest.getEmail());
             if (staff == null) {
                 return ResponseEntity.status(HttpStatus.UNAUTHORIZED)
-                    .body(new LoginResponse(false, "Email does not exist"));
+                    .body(new LoginResponse(false, "Email does not exist", null, null, null));
             }
             if (!verifyPassword(loginRequest.getPassword(), staff.getStaffpassword())) {
                 return ResponseEntity.status(HttpStatus.UNAUTHORIZED)
-                    .body(new LoginResponse(false, "Incorrect password"));
+                    .body(new LoginResponse(false, "Incorrect password", null, null, null));
             }
-            return ResponseEntity.ok(new LoginResponse(true, "Staff login successful"));
+            return ResponseEntity.ok(new LoginResponse(true, "Staff login successful", 
+                staff.getStaffemail(), staff.getStaffid(), staff.getStaffpassword()));
         } catch (SQLException e) {
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
-                .body(new LoginResponse(false, "An error occurred"));
+                .body(new LoginResponse(false, "An error occurred", null, null, null));
         }
     }
 
--- src/main/java/com/heroku/java/bean/LoginResponse.java
+++ src/main/java/com/heroku/java/bean/LoginResponse.java
@@ -1,8 +1,49 @@
 package com.heroku.java.bean;
 
 public class LoginResponse {
+    
     private boolean success;
     private String message;
+    private String email;
+    private Integer id;
+    private String password;
+
+    public LoginResponse(boolean success, String message, String email, Integer id, String password) {
+        this.success = success;
+        this.message = message;
+        this.email = email;
+        this.id = id;
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+
+    public Integer getId() {
+        return id;
+    }
+
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+
+    public String getPassword() {
+        return password;
+    }
+
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 
     public boolean isSuccess() {
         return success;
@@ -17,11 +58,6 @@ public class LoginResponse {
     }
 
     public void setMessage(String message) {
-        this.message = message;
-    }
-
-    public LoginResponse(boolean success, String message) {
-        this.success = success;
         this.message = message;
     }
 }
