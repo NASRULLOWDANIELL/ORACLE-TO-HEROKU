b96c3e312330eb7c75dd94d4016caba0db15a99f
NASRULLOWDANIELL <2022456146@student.uitm.edu.my>
2024-07-30 14:37:25 +0800

    cscs

--- src/main/java/com/heroku/java/controller/BusinessController.java
+++ src/main/java/com/heroku/java/controller/BusinessController.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import com.fasterxml.jackson.databind.JsonNode;
 import com.heroku.java.bean.Business;
 import com.heroku.java.service.BusinessService;
 
@@ -23,14 +24,14 @@ public class BusinessController {
     @GetMapping("/syujaahBusiness")
     public String syujaahBusiness(Model model) {
         try {
-            List<Business> businesses = businessService.fetchBusinesses();
+            List<JsonNode> businesses = businessService.fetchBusinesses();
             model.addAttribute("businesses", businesses);
         } catch (Exception e) {
-            // Log the error
             e.printStackTrace();
-            // Add an error message to the model
             model.addAttribute("error", "Unable to fetch business data. Please try again later.");
         }
         return "businessPage";
     }
+
+    
 }
